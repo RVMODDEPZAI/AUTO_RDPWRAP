9dc898df63a39774375a2d398c5f24d323b5ff40
RVMODDEPZAI <rvmod03@gmail.com>
2025-06-24 12:54:03 +0700

    autordpwrap.bat

---
+++ autordpwrap.bat
@@ -0,0 +1,47 @@
+@echo off
+title AutoRDPWRAP By @RVMOD_VN
+color 0A
+echo AutoRDPWRAP By @RVMOD_VN
+set /p userinput=Do you want to use? (Y/N): 
+
+if /i "%userinput%" NEQ "Y" (
+    echo Operation cancelled by user.
+    timeout /t 2 >nul
+    exit
+)
+
+echo.
+echo Preparing to download file...
+echo.
+
+powershell -Command ^
+$ProgressPreference = 'Continue'; ^
+$url = 'https://github.com/RVMODDEPZAI/AUTO_RDPWRAP/raw/refs/heads/main/RDPWrap.zip'; ^
+$output = $env:TEMP + '\RDPWrap.zip'; ^
+Invoke-WebRequest -Uri $url -OutFile $output
+
+echo.
+echo Download completed!
+echo Extracting file...
+powershell -Command "Expand-Archive -Path $env:TEMP\RDPWrap.zip -DestinationPath $env:TEMP -Force"
+echo Extraction completed!
+
+echo.
+echo Running install.bat...
+call "%TEMP%\RDPWrap\install.bat"
+timeout /t 30 >nul
+
+echo.
+echo Moving files to C:\Program Files\RDP Wrapper...
+if not exist "C:\Program Files\RDP Wrapper" mkdir "C:\Program Files\RDP Wrapper"
+xcopy /y /e /i "%TEMP%\RDPWrap\*" "C:\Program Files\RDP Wrapper\"
+
+echo.
+echo Running autoupdate.bat...
+call "C:\Program Files\RDP Wrapper\autoupdate.bat"
+timeout /t 30 >nul
+
+echo.
+echo All tasks completed successfully!
+timeout /t 3 >nul
+exit
